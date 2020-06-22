b22ed7e7fb8a0b7d29e5834c624a5c1ea234e03a
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-06-22 15:39:32 -0400

    s02 client updates

--- s02_blog/client/src/CommentList.js
+++ s02_blog/client/src/CommentList.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
 export default ({ comments }) => {
   const renderedComments = comments.map(comment => {
     let content;
 
-    if (comment.status === "approved") {
+    if (comment.status === 'approved') {
       content = comment.content;
     }
 
-    if (comment.status === "pending") {
-      content = "Awaiting comment approval";
+    if (comment.status === 'pending') {
+      content = 'This comment is awaiting moderation';
     }
 
-    if (comment.status === "rejected") {
-      content = "Comment rejected";
+    if (comment.status === 'rejected') {
+      content = 'This comment has been rejected';
     }
 
     return <li key={comment.id}>{content}</li>;
