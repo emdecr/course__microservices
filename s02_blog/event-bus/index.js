4d471e62015b4c9a3d9b9194834d2541dc5a5f06
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-06-22 15:36:46 -0400

    s02 wrapup

--- s02_blog/event-bus/index.js
+++ s02_blog/event-bus/index.js
@@ -5,13 +5,22 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res, next) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
   axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4003/events", event);
 
+  res.status(200).send({ status: "OK" });
+});
+
+app.get("events", (req, res, next) => {
   res.status(200).send({ status: "OK" });
 });
 
